e42fa2d3d1ec656a9ef69999de07433a50c1d840
unkownnnn67959 <125199468+unkownnnn67959@users.noreply.github.com>
2023-09-09 18:34:40 +0300

    Update main.py
    cw6

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # write your code here
+person = {
+    "name": "sara",
+    "age": 17,
+    "hobbies" : ['karate' ,'cooking', 'reading' ],
+}
+
+print(person['name'])
+print(person['age'])
+
+person["country"] = "italy"
+print(person)
+print(len(person))
+
+person["hobbies"] = "swimming"
+def check_hobbies(person):
+    if len(person["hobbies"]) >=3:
+        print("wow you are amazing")
+
+
+
